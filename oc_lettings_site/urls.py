f8d6f6c5e36393deefb66ace1dfdb856df713571
MeldSnake <ikeek.33@gmail.com>
2023-05-24 19:24:33 +0200

    Ajout d'une url pour sentry
    Signed-off-by: MeldSnake <ikeek.33@gmail.com>

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,7 +3,13 @@ from django.urls import path, include
 
 from . import views
 
+
+def trigger_error():
+    _ = 12 / 0
+
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('', views.index, name='index'),
     path('lettings/', include("lettings.urls", namespace="lettings")),
     path('profiles/', include("profiles.urls", namespace="profiles")),
